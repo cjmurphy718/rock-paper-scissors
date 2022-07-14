67f8c01674b3ec336425324ab1993b50f4097c07
cjmurphy718 <cjp449@stern.nyu.edu>
2022-07-14 08:43:49 -0500

    User inputs
    Processing user inputs

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rock-paper-scissors
+```sh
+python game.py
+```
+
+
+```py
+print("HELLO"
+```
---
+++ game.py
@@ -0,0 +1,21 @@
+# this is the "game.py" file...
+
+print("Rock, Paper, Scissors, Shoot!")
+
+
+# USER INPUTS
+
+user_input = input("Please make a selection ('rock', 'paper', 'scissors)")
+# You chose: 'rock'
+print (f"You chose: '{user_choice}")
+
+# VALIDATE USER INPUTS
+
+
+
+# COMPUTER CHOICE
+
+
+
+# DETERMINE THE WINNER
+
