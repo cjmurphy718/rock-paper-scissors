3ed5278b5b7a949d2facf9fedfa8ff5245ff75a4
cjmurphy718 <cjp449@stern.nyu.edu>
2022-07-14 09:11:02 -0500

    Input computer choices

--- game.py
+++ game.py
@@ -1,11 +1,13 @@
 # this is the "game.py" file...
+import random
+
 
 print("Rock, Paper, Scissors, Shoot!")
 
 
 # USER INPUTS
 
-user_input = input("Please make a selection ('rock', 'paper', 'scissors)")
+user_input = input("Please make a selection ('rock', 'paper', 'scissors')")
 # You chose: 'rock'
 print (f"You chose: '{user_choice}")
 
@@ -14,6 +16,9 @@ print (f"You chose: '{user_choice}")
 
 
 # COMPUTER CHOICE
+valid_options=["rock","paper","scissors"]
+computer_choice = random.choice(valid_options)
+print("Computer chose:", computer_choice)
 
 
 
