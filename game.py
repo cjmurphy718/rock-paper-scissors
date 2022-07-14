5139acdb04644a2564bdaca7620953080d536184
cjmurphy718 <cjp449@stern.nyu.edu>
2022-07-14 13:59:18 -0500

    Updated error in user inputs

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ print("Rock, Paper, Scissors, Shoot!")
 
 user_input = input("Please make a selection ('rock', 'paper', 'scissors')")
 # You chose: 'rock'
-print (f"You chose: '{user_choice}' ")
+print(f"You chose: '{user_input}' ")
 
 # VALIDATE USER INPUTS
 
@@ -28,16 +28,16 @@ if user_input==computer_choice:
     print("Tie game!")
 elif user_input=="paper":
     if computer_choice=="Scissors":
-    print("Scissors cut paper. You lose!")
+        print("Scissors cut paper. You lose!")
     else:
         print("Paper covers rock. You win!")
 elif user_input=="scissors":
     if computer_choice=="Paper":
-    print("Scissors cut paper. You win!")
+        print("Scissors cut paper. You win!")
     else:
         print("Rock crushes scissors. You lose!")
 elif user_input=="rock":
     if computer_choice=="scissors":
-    print("Rock crushes scissors. You win!")
+        print("Rock crushes scissors. You win!")
     else:
         print("Paper covers rock. You lose!")
